c4acf4c0d12d4dd3f66218a78d8b01646b11c538
VinceLiuzy <vince.liuzy@outlook.com>
2021-10-21 23:12:35 +0800

    init
    Signed-off-by: VinceLiuzy <vince.liuzy@outlook.com>

--- mercury-web/src/env.js
+++ mercury-web/src/env.js
@@ -1,10 +1,10 @@
 (function (window) {
   window._env = window._env || {};
 
-  window._env.server = 'http://114.132.242.164:8190/';
+  window._env.server = 'http://114.132.242.164:9800/';
 
   // API url
-  window._env.apiUrl = window._env.server + 'mercury-boot-api/api/';
+  window._env.apiUrl = window._env.server + 'mercury-boot-api/';
   window._env.uploadUrl = window._env.server + 'mercury-boot-api/minio/upload';
   window._env.downloadUrl = window._env.server + 'mercury-boot-api/minio/download';
 
